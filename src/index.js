e223dd77a2a569868a8928dfcd728a752696d532
hiro881213 <nikep.godbard@gmail.com>
2022-02-27 19:49:27 +0900

    コンポーネント分割

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import ReactDOM from "react-dom";
-
-const App = () => {
-    // return <h1>こんにちは</h1>;
-
-    // return (
-    //     <div>
-    //         <h1>こんにちは</h1>
-    //         <p>お元気ですか？</p>
-    //     </div>
-    // );
-
-    return (
-        <>
-            <h1>こんにちは</h1>
-            <p>お元気ですか？</p>
-        </>
-    );
-
-
-};
+import {App} from "./App";
 
 // ReactDOMを描画する
 ReactDOM.render(<App />,document.getElementById("root"));
