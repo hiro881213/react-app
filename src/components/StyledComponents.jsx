5473555011c9bcd136b0764cf2fe4f1a93e4e5a7
hiro881213 <nikep.godbard@gmail.com>
2022-03-05 21:47:08 +0900

    styled-jsxの実行

--- src/components/StyledComponents.jsx
+++ src/components/StyledComponents.jsx
@@ -1,10 +1,41 @@
+import styled from "styled-components";
+
 export const StyledComponents = () => {
 
     return (
-        <div>
-            <p>styled componentsです</p>
-            <button>ボタン</button>
-        </div>
-    )
+        <SContainer>
+            <STitle>styled componentsです</STitle>
+            <SButton>ボタン</SButton>
+        </SContainer>
+    );
 
-}
+};
+
+const SContainer = styled.div`
+    border: solid 1px #F66;
+    border-radius: 20px;
+    padding: 8px;
+    margin: 8px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+const STitle = styled.p`
+    margin: 0;
+    color: #F44;
+`;
+
+const SButton = styled.button`
+    background-color: #FAA;
+    border: none;
+    padding: 8px;
+    border-radius: 8px;
+    color: #444;
+    &:hover {
+        background-color: #F44;
+        color: #FFF;
+        cursor: pointer;
+    }
+`;
+
