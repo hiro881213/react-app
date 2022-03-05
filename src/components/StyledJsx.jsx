3fc502d3bf94e450ab5f8409e399043228379950
hiro881213 <nikep.godbard@gmail.com>
2022-03-05 21:01:23 +0900

    styled-jsx

--- src/components/StyledJsx.jsx
+++ src/components/StyledJsx.jsx
@@ -1,10 +1,44 @@
 export const StyledJsx = () => {
 
     return (
-        <div>
-            <p>Styled JSXです</p>
-            <button>ボタン</button>
-        </div>
+        <>
+            <div className = 'container'>
+                <p className = 'title'>Styled JSXです</p>
+                <button className = 'botton'>ボタン</button>
+            </div>
+            <style jsx>
+            {`
+                .container {
+                    border: solid 1px #AFA;
+                    border-radius: 20px;
+                    padding: 8px;
+                    margin: 8px;
+                    display: flex;
+                    justify-content: space-around;
+                    align-items: center;
+                }
+
+                .title {
+                    margin: 0;
+                    color: #3F3;
+                }
+
+                .botton {
+                    background-color: #AFA;
+                    border: none;
+                    padding: 8px;
+                    border-radius: 8px;
+                    color: #666;
+                }
+                
+                .botton:hover {
+                    background-color: #0A0;
+                    color: #FFF;
+                }
+
+            `}
+            </style>
+        </>
     );
 
 };
