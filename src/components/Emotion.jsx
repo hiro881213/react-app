9ca7d2ccb6935e5d98497c285dcad1eaad9fc1d8
hiro881213 <nikep.godbard@gmail.com>
2022-03-05 22:40:20 +0900

    Emotionの利用3

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import styled from "@emotion/styled"
 
 export const Emotion = () => {
     
@@ -24,8 +25,22 @@ export const Emotion = () => {
     return (
         <div css={containerStyle}>
             <p css = {titleStyle}>Emotionです</p>
-            <button>ボタン</button>
+            <SButton>ボタン</SButton>
         </div>
     );
 
 };
+
+// styled-comoinentsの書き方
+const SButton = styled.button`
+    background-color: #AAF;
+    border :none;
+    padding: 8px;
+    border-radius: 8px;
+    color: #666;
+    &: hover {
+        background-color: #33F;
+        color: #FFF;
+        cursor: pointer;
+    }
+`;
