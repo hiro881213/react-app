c67da7e9ead6e6450f9c1098be9831e6b56c17b1
hiro881213 <nikep.godbard@gmail.com>
2022-03-05 22:11:06 +0900

    Emotionの利用1

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,8 +1,21 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 export const Emotion = () => {
     
+    // SCSSの書き方がそのまま可能な書き方
+    const containerStyle = css`
+        border: solid 1px #aaa;
+        border-radius: 20px;
+        padding: 8px;
+        margin: 8px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    `;
+
     return (
-        <div>
+        <div css={containerStyle}>
             <p>Emotionです</p>
             <button>ボタン</button>
         </div>
