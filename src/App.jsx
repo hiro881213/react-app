911adebf0151e14a847c401562e33481d2cbe473
hiro881213 <nikep.godbard@gmail.com>
2022-03-06 17:12:57 +0900

    ページ遷移処理作成1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { ColoredMessage } from './components/ColoredMessage';
-import { useState } from 'react';
+import React from 'react'
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import page1 from './page1';
 
-import { CssModules } from './components/CssModules';
-import { StyledJsx } from './components/StyledJsx';
-import { StyledComponents } from './components/StyledComponents'
-import { Emotion } from './components/Emotion';
-import { Tailwind } from './components/Tailwind';
+// import { ColoredMessage } from './components/ColoredMessage';
+// import { useState } from 'react';
+
+// import { CssModules } from './components/CssModules';
+// import { StyledJsx } from './components/StyledJsx';
+// import { StyledComponents } from './components/StyledComponents'
+// import { Emotion } from './components/Emotion';
+// import { Tailwind } from './components/Tailwind';
 
 import './App.scss';
 
-function App() {
 
-    // Stateの定義
-    const [num, setNum] = useState(0);
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        HelloPage1!!
+      </div>
+    );
+  }
+}
+
+// function App() {
+
+    // // Stateの定義
+    // const [num, setNum] = useState(0);
 
     // // numが変化したときの処理
     // useEffect(() => {
@@ -20,35 +35,40 @@ function App() {
     // }, [num])
 
 
-    // ボタンクリック処理
-    const onClickButton = () => {
+    // // ボタンクリック処理
+    // const onClickButton = () => {
 
-        // カウントアップする
-        setNum(num + 1);
+    //     // カウントアップする
+    //     setNum(num + 1);
 
-    };
+    // };
 
-  return (
-    <>
-    <div className="App">
-      <main>
-        <h1 style = {{ color: "red" }}>こんにちは</h1>
+  // return (
+  //   <>
+  //   <div className="App">
+  //     <main>
+  //       {/* <BrowserRouter>
+  //         <Switch>
+  //           <Route exact path = "/" component = {page1} />
+  //         </Switch>
+  //       </BrowserRouter> */}
+  //       {/* <h1 style = {{ color: "red" }}>こんにちは</h1>
 
-        <ColoredMessage color = "blue">お元気ですか？</ColoredMessage>
-        <ColoredMessage color = "pink">元気です</ColoredMessage>
+  //       <ColoredMessage color = "blue">お元気ですか？</ColoredMessage>
+  //       <ColoredMessage color = "pink">元気です</ColoredMessage>
 
-        <button onClick={onClickButton}>カウントアップ</button>
-        <p>{num}</p>
+  //       <button onClick={onClickButton}>カウントアップ</button>
+  //       <p>{num}</p>
         
-        <CssModules />
-        <StyledJsx />
-        <StyledComponents />
-        <Emotion />
-        <Tailwind />
-      </main>
-    </div>
-    </>
-  );
-}
+  //       <CssModules />
+  //       <StyledJsx />
+  //       <StyledComponents />
+  //       <Emotion />
+  //       <Tailwind /> */}
+  //     </main>
+  //   </div>
+  //   </>
+  // );
+// }
 
 export default App;
