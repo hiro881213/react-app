5e20c3530b366a15f9edb4d1dbd6a975d07f53b6
hiro881213 <nikep.godbard@gmail.com>
2022-03-07 21:58:33 +0900

    Usecontextの利用

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.scss';
+
 import App from './App';
 
+import { AdminFlagProvider } from "./components/chap7_child/providers/AdminFlagProvider";
+
 // ReactDOMを描画する
-ReactDOM.render(<App />,document.getElementById("root"));
+ReactDOM.render(
+    <AdminFlagProvider>
+        <App />
+    </AdminFlagProvider>,
+    document.getElementById("root")
+);
